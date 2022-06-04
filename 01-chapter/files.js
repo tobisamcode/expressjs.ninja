d618e5bcae04bded24f2093ea9e7b100bbe86de7
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-04 16:47:32 +0100

    basics

--- 01-chapter/files.js
+++ 01-chapter/files.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 // reading files
-fs.readFile("./docs/blog1.txt", (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data.toString());
-});
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(data.toString());
+// });
 
 // console.log("last line");
 
