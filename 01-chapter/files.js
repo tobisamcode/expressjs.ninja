7a0ff02ca2df0e698cee87399590938628bddb19
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-04 16:27:29 +0100

    basics

--- 01-chapter/files.js
+++ 01-chapter/files.js
@@ -1,6 +1,18 @@
+const fs = require("fs");
 // reading files
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(data.toString());
+// });
+
+// console.log("last line");
 
 // writing files
+fs.writeFile("./docs/blog1.txt", "hello world", () => {
+  console.log("already written in the file");
+});
 
 // directories
 
