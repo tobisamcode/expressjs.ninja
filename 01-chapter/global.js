3fec94408c05489bea518710e28f380a5525688c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-04 15:48:07 +0100

    global.js

---
+++ 01-chapter/global.js
@@ -0,0 +1,15 @@
+// Global Object
+
+// console.log(global);
+
+// setTimeout(() => {
+//   console.log("in the timeout for 3 seconds");
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log("in the interval every 1s");
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
