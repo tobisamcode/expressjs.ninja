b92ed3ec9166dfb9be0f5a47c05c9fbdff24a516
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-04 22:10:59 +0100

    delete

--- 01-chapter/files.js
+++ 01-chapter/files.js
@@ -34,3 +34,11 @@ if (!fs.existsSync("./assets")) {
 }
 
 // deleting files
+if (fs.existsSync("./docs/deleteme.txt")) {
+  fs.unlink("./docs/deleteme.txt", err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("file deleted");
+  });
+}
