f38ed07878eba0b272d8bb4bba509c4c37bd85b6
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-04 16:46:22 +0100

    readMe

--- 01-chapter/README.md
+++ 01-chapter/README.md
@@ -5,3 +5,11 @@ a chapter that explains the basics and the system of nodejs.
 - operating system ('os')
 - module.exports = {}
 - require('')
+
+// reading files
+fs.readFile("./docs/blog1.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
+});
--- 01-chapter/files.js
+++ 01-chapter/files.js
@@ -1,19 +1,29 @@
 const fs = require("fs");
 // reading files
-// fs.readFile("./docs/blog1.txt", (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(data.toString());
-// });
+fs.readFile("./docs/blog1.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString());
+});
 
 // console.log("last line");
 
 // writing files
-fs.writeFile("./docs/blog1.txt", "hello world", () => {
-  console.log("already written in the file");
-});
+// fs.writeFile("./docs/blog1.txt", "hello world", () => {
+//   console.log("already written in the file");
+// });
+
+// fs.writeFile("./docs/blog2.txt", "hello again", () => {
+//   console.log("already written in the file");
+// });
 
 // directories
+fs.mkdir("./assets", err => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("folder created");
+});
 
 // deleting files
