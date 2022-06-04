8038951a6f61053687d3c327ceb7fbe5db1b451c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-04 23:00:46 +0100

    created a server

--- 02-chapter/server.js
+++ 02-chapter/server.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  res.write("<p>hello, ninjas</p>");
+  res.write("<p>hello again, ninjas</p>");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
