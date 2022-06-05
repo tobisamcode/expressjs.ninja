5011602a8ec45227fae9aaba54b091768768195b
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-05 13:00:14 +0100

    npm init setup

--- 02-chapter/app.js
+++ 02-chapter/app.js
@@ -13,6 +13,17 @@ app.get("/about", (req, res) => {
   res.sendFile("./views/about.html", { root: __dirname });
 });
 
+// redirects
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
+
 // listen for request
 app.listen(3000, () => {
   console.log("server runing");
