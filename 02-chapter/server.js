b26eddc0c0c2af22399e64c8812fdbb156eafc99
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-05 12:08:32 +0100

    npm init setup

--- 02-chapter/server.js
+++ 02-chapter/server.js
@@ -2,8 +2,11 @@ const http = require("http");
 
 const fs = require("fs");
 
+const _ = require("lodash");
+
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  // lodash
+  const num = _.random(0, 20);
 
   // set header content type
   res.setHeader("Content-Type", "text/html");
@@ -18,7 +21,7 @@ const server = http.createServer((req, res) => {
       path += "about.html";
       res.statusCode = 200;
       break;
-    case "/about-me":
+    case "/about-us":
       res.statusCode = 301;
       res.setHeader("Location", "/about");
       res.end();
