6e9814cef62dfab2b0280441dd563abe840cd2fb
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-13 12:25:17 +0100

    creating models and schema

--- 02-chapter/server.js
+++ 02-chapter/server.js
@@ -89,6 +89,11 @@ app.post("/blogs", (req, res) => {
     .catch(err => console.log(err));
 });
 
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+});
+
 app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "Create a new blog" });
 });
