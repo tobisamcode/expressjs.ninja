457d1da8b3b049e820d1492c8fbded5b2d58f5b8
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-06-13 12:19:07 +0100

    creating models and schema

--- 02-chapter/server.js
+++ 02-chapter/server.js
@@ -27,6 +27,7 @@ app.set("view engine", "ejs");
 
 // middlewares and static files
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 // mongoose nad mongo sandbox routes
@@ -76,6 +77,15 @@ app.get("/blogs", (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
     .then(result => res.render("index", { title: "All Blogs", blogs: result }))
+    .catch(err => console.log(err));
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then(result => res.redirect("/blogs"))
     .catch(err => console.log(err));
 });
 
